4a7d52da3c2ea16c30bd152389781028ca0fcc00
Jaya Agrawal <kanchhaljaya@gmail.com>
2026-08-11 13:18:37 +0530

    linearSearch

---
+++ DSA/Arrays/linearSearch.java
@@ -0,0 +1,17 @@
+class linearSearch {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 25, 7, 40, 15};
+        int target = 40;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                return;
+            }
+        }
+
+        System.out.println("Element not found");
+    }
+}
