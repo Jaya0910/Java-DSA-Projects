b1da4b77689ce48e5a9da687f9e91344d4507f85
Jaya Agrawal <kanchhaljaya@gmail.com>
2026-08-13 11:06:43 +0530

    innvalid Num exception

---
+++ JavaPractice/exception/invalidNum.java
@@ -0,0 +1,11 @@
+class invalidNum{
+    public static void main(String[] args) {
+        String str = "abc";
+        try {
+            int num = Integer.parseInt(str);
+            System.out.println("Parsed number: " + num);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Invalid number format.");
+        }
+    }
+}
